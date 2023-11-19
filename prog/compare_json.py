a771659925750de9bac03df3f849138ffbc6a00a
koritalasuryateja <151409464+koritalasuryateja@users.noreply.github.com>
2023-11-18 21:20:22 -0500

    Update compare_json.py

--- prog/compare_json.py
+++ prog/compare_json.py
@@ -37,6 +37,5 @@ def main():
         print(f"Error parsing JSON: {e}", file=sys.stderr)
         sys.exit(2)  # Exit with a non-zero status to indicate an error
 
-if _name_ == '_main_':
+if __name__ == '__main__':
     main()
-
