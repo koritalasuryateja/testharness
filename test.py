13e2e984049228c5270302e7f4f48717df1f0afe
koritalasuryateja <151409464+koritalasuryateja@users.noreply.github.com>
2023-11-18 19:24:50 -0500

    Update test.py

--- test.py
+++ test.py
@@ -9,9 +9,9 @@ class TestProgram(unittest.TestCase):
     def run_test(self, program, test_name, use_args=False):
         input_file = f'{self.test_dir}/{program}.{test_name}.in'
         expected_file = f'{self.test_dir}/{program}.{test_name}' + ('.arg.out' if use_args else '.out')
-
+        
         # If use_args is True, the input should be passed as a command-line argument
-        cmd = [os.path.join(self.prog_dir, f"{program}.py")]
+        cmd = ['python', os.path.join(self.prog_dir, f"{program}.py")]
         if use_args:
             cmd.append(input_file)
         else:
@@ -19,20 +19,20 @@ class TestProgram(unittest.TestCase):
                 process = subprocess.run(cmd, stdin=f, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
                 self.assertEqual(process.returncode, 0)
                 actual_output = process.stdout.decode('utf-8')
-
+        
         # When use_args is True, we need to execute the program again
         # as some programs might behave differently when using arguments vs. stdin
         if use_args:
             process = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
             self.assertEqual(process.returncode, 0)
             actual_output = process.stdout.decode('utf-8')
-
+        
         with open(expected_file, 'r') as f:
             expected_output = f.read().strip()
-
+        
         self.assertEqual(actual_output.strip(), expected_output,
                          f'Failed test: {program}.{test_name} with {"arguments" if use_args else "stdin"}')
-
+    
     def test_compare_json(self):
         self.run_test('compare_json', 'test1')
         self.run_test('compare_json', 'test2')
